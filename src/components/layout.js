39da013bac9b93619bd30bf7123f435ad2d7fcce
Runkai Zhang <1105420698@qq.com>
2020-02-10 13:10:40 -0500

    Fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,8 +7,6 @@ import { rhythm } from '../utils/typography'
 import 'typeface-montserrat'
 import 'typeface-merriweather'
 
-import '../styles/layout.css'
-
 export default ({ children, location }) => (
   <StaticQuery
     query={graphql`
